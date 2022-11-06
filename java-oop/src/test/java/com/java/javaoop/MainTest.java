414caa4fbb5f690877534ab9f438ef32036cd33e
heechul <khc4190@naver.com>
2022-11-06 21:29:04 +0900

    메인 구현

---
+++ java-oop/src/test/java/com/java/javaoop/MainTest.java
@@ -0,0 +1,18 @@
+package com.java.javaoop;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void main() {
+        // Given
+        String[] args = {"3", "1", "2"};
+
+        // When
+        Main.main(args);
+
+        // Then
+
+    }
+}
